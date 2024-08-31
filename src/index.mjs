eeb8f7a7de0e83725c12ec56b296045b64bf34b6
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-09-01 01:02:36 +0600

    get request

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.status(201).send({ msg: "Hello" });
+});
+
+app.get("/api/users", (req, res) => {
+    res.send([
+        { id: 1, username: "Mitul", displayName: "Abu Al Nasir" },
+        { id: 2, username: "Niloy", displayName: "Niloy Kumar" },
+        { id: 3, username: "Mahfuz", displayName: "Mahfuzur Rahman" },
+    ]);
+});
+
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 });
