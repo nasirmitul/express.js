ccb089f710b39200d68cce19342ed5bcd3825dc1
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-09-03 07:34:50 +0600

    put request

--- src/index.mjs
+++ src/index.mjs
@@ -36,8 +36,8 @@ app.get("/api/users", (req, res) => {
 //post request
 app.post("/api/users", (req, res) => {
     console.log(req.body);
-    const {body} = req;
-    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body};
+    const { body } = req;
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
     mockUsers.push(newUser)
     return res.status(201).send(newUser)
 })
@@ -53,6 +53,26 @@ app.get('/api/users/:id', (req, res) => {
     const findUser = mockUsers.find((user) => user.id === parsedId)
     if (!findUser) return res.sendStatus(404);
     return res.send(findUser);
+})
+
+
+//put request
+app.put("/api/users/:id", (req, res) => {
+    const {
+        body,
+        params: { id },
+    } = req;
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) return res.sendStatus(400);
+
+    const findUserIndex = mockUsers.findIndex(
+        (user) => user.id === parsedId
+    );
+
+    if (findUserIndex === -1) return res.sendStatus(404);
+
+    mockUsers[findUserIndex]= {id: parsedId, ...body};
 })
 
 app.listen(PORT, () => {
